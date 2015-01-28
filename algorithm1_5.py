c7fd085fed9d4e8d5a23ea7744edcf5b01d370b8
FeiHuang2012 <root@fh.(none)>
2015-01-28 22:55:43 +0800

    Replace ch1 with ch2 in str1

---
+++ algorithm1_5.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+'''
+Write a method to replace all spaces in a string with '%20'
+'''
+import sys
+
+def replaceStr(str1, ch1, ch2):
+    if ch1 not in str1:
+        return false
+    listStr1 = list(str1)
+    _lenListStr1 = len(listStr1)
+    for i in range(_lenListStr1):
+        if listStr1[i-1] == ch1:
+            listStr1[i-1] = ch2
+    str1List = ''.join(listStr1)
+    return str1List
+
+if __name__ == "__main__":
+    str1 = sys.argv[1]
+    ch1 = sys.argv[2]
+    ch2 = sys.argv[3]
+    print replaceStr(str1, ch1, ch2)
+            
