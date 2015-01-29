986294553154d7d4dcee5e5676ecb9a8cccd6f40
FeiHuang2012 <root@fh.(none)>
2015-01-29 18:10:05 +0800

    Set row and colume to 0 while the element in MxN matrix is 0

---
+++ algorithm1_7.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+'''
+Write an algorithm such that if an element in an MxN matrix is 0, its entire row and column is set to 0
+ex:
+[1,2,3,4]     [1,0,3,4] 
+[4,0,6,5]  => [0,0,0,0]
+[7,8.9,6]     [7,0,9,6]
+'''
+import sys
+
+
